b08b08b66589169d56d3cf24604c63b49e522eb3
leehyeonmin34 <leehyeonmin34@gmail.com>
2023-06-29 11:17:34 +0900

    asyncLoadInfoBatch 수정

--- src/main/java/com/leehyeonmin34/weather_reminder/global/batch/load_info/AsyncLoadInfoBatch.java
+++ src/main/java/com/leehyeonmin34/weather_reminder/global/batch/load_info/AsyncLoadInfoBatch.java
@@ -132,7 +132,6 @@ public class AsyncLoadInfoBatch {
                 .build();
         itemWriter.afterPropertiesSet();
 
-//        return itemWriter;
         return new JdbcBatchListItemWriter<>(itemWriter);
     }
 
