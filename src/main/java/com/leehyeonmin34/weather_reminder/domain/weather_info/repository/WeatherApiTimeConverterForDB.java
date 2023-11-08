e0c5aafe2c81803c32115fd13bb45971c1f73c2b
leehyeonmin34 <leehyeonmin34@gmail.com>
2023-11-08 21:41:37 +0900

    #13 weather_info의 날짜정보타입을 String에서 Timestamp로 변경

--- src/main/java/com/leehyeonmin34/weather_reminder/domain/weather_info/repository/WeatherApiTimeConverterForDB.java
+++ src/main/java/com/leehyeonmin34/weather_reminder/domain/weather_info/repository/WeatherApiTimeConverterForDB.java
@@ -1,26 +1,28 @@
 package com.leehyeonmin34.weather_reminder.domain.weather_info.repository;
 
 import com.leehyeonmin34.weather_reminder.domain.weather_info.service.WeatherApiTimeStringConverter;
+import com.leehyeonmin34.weather_reminder.domain.weather_info.service.WeatherApiTimeTimestampConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Converter
 @Component
 @RequiredArgsConstructor
-public class WeatherApiTimeConverterForDB implements AttributeConverter<LocalDateTime, String> {
+public class WeatherApiTimeConverterForDB implements AttributeConverter<LocalDateTime, Timestamp> {
 
 
     @Override
-    public String convertToDatabaseColumn(final LocalDateTime attribute) {
-        return WeatherApiTimeStringConverter.serialize(attribute);
+    public Timestamp convertToDatabaseColumn(final LocalDateTime attribute) {
+        return WeatherApiTimeTimestampConverter.serialize(attribute);
     }
 
     @Override
-    public LocalDateTime convertToEntityAttribute(final String dbData) {
-        return WeatherApiTimeStringConverter.parse(dbData);
+    public LocalDateTime convertToEntityAttribute(final Timestamp dbData) {
+        return WeatherApiTimeTimestampConverter.parse(dbData);
     }
 }
